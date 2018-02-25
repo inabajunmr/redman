342038437531969e5585dd386a1a336b4a964fcd
ishikawa <ishikawa.masayuki.0605@gmail.com>
2018-02-25 11:58:21 +0900

    いろいろ手直し

---
+++ src/main/java/xyz/dgz48/redman/Application.java
@@ -0,0 +1,33 @@
+package xyz.dgz48.redman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * メインクラス.
+ *
+ * @author win2cot
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * コンストラクタ.<BR>
+	 * <BR>
+	 * checkstyle - HideUtilityClassConstructorを回避<BR>
+	 * http://checkstyle.sourceforge.net/config_design.html#HideUtilityClassConstructor#HideUtilityClassConstructor
+	 */
+	protected Application() {
+		// nop
+	}
+
+	/**
+	 * 実行エントリポイント.
+	 *
+	 * @param args 実行時引数
+	 */
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
--- src/test/java/xyz/dgz48/redman/ApplicationTests.java
+++ src/test/java/xyz/dgz48/redman/ApplicationTests.java
@@ -5,12 +5,22 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * メインクラスのテストクラス.
+ *
+ * @author win2cot
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RedmanApplicationTests {
+public class ApplicationTests {
 
+	/**
+	 * ApplicationContextロードのテスト.
+	 */
 	@Test
 	public void contextLoads() {
+		// nop
 	}
 
 }
