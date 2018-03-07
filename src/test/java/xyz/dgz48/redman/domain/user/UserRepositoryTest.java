c233fe472ff38f9ccd79cc05ad0c2b9c45a79545
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-08 02:58:33 +0900

    import

--- src/test/java/xyz/dgz48/redman/domain/user/UserRepositoryTest.java
+++ src/test/java/xyz/dgz48/redman/domain/user/UserRepositoryTest.java
@@ -1,22 +1,10 @@
 package xyz.dgz48.redman.domain.user;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import xyz.dgz48.redman.Application;
-
-
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
