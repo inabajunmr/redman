666a4bb24ce30e22bc98d45dfaf5e3a426a87d5c
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-11 00:41:39 +0900

    javadoc

--- src/main/java/xyz/dgz48/redman/domain/user/UserFactory.java
+++ src/main/java/xyz/dgz48/redman/domain/user/UserFactory.java
@@ -12,18 +12,18 @@ import org.springframework.stereotype.Component;
 public class UserFactory {
 
 	/**
-	 * Create {@User} by {@UserEntity}.
+	 * Create {@link User} by {@link UserEntity}.
 	 * @param entity user entity
-	 * @return {@User}
+	 * @return {@link User}
 	 */
 	public Optional<User> create(final Optional<UserEntity> entity) {
 		return entity.map(e -> new User(e.getUserId(), e.getIdpUserName(), e.getIdpType()));
 	}
 
 	/**
-	 * Create {@User} by {@UserEntity}.
+	 * Create {@link User} by {@link UserEntity}.
 	 * @param entity user entity
-	 * @return {@User}
+	 * @return {@link User}
 	 */
 	public User create(final UserEntity entity) {
 		return new User(entity.getUserId(), entity.getIdpUserName(), entity.getIdpType());
