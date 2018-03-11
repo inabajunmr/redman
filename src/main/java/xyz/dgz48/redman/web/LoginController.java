e75f076778b139a6685353b79e7a37f1b6c180c4
inabajunmr <inabajun.for.regi@gmail.com>
2018-03-11 19:53:49 +0900

    config for bootRun

--- src/main/java/xyz/dgz48/redman/web/LoginController.java
+++ src/main/java/xyz/dgz48/redman/web/LoginController.java
@@ -1,24 +1,39 @@
 package xyz.dgz48.redman.web;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import xyz.dgz48.redman.domain.user.User;
+import xyz.dgz48.redman.domain.user.UserService;
 
 
 /**
  * Controller for login.
  */
 @Controller
+@Slf4j
 public class LoginController {
 
     /**
-     * 認証後に表示するページ。最終的にリソースの一覧か何かになると思う.
+     * Service for {@link User}.
+     */
+    @Autowired
+    UserService userService;
+
+    /**
+     * 認証後に表示するページ。
      *
      * @param authentication authentication info
      * @return user info
      */
     @GetMapping("/")
     public String index(final OAuth2AuthenticationToken authentication) {
+        log.info("Login user:{}", authentication.getName());
+//        String name = authentication.getName();
+//
+//        userService.findUserByIdpUserName( );
         return "index";
     }
 
